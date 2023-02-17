009c6b6973f4618ed02804777de8f1d73d357a31
Alonso <alonso.burgos.astorga@gmail.com>
2023-02-17 13:36:40 -0700

    [feat]: adding palette colors, and making some css config to start working with components

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Header } from "./components/Header/Header";
+
 function App() {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-  return (
-    <div>
-      <div>THIS IS THE MELI CHALLENGE</div>
-      <div>
-        <p>{data}</p>
-      </div>
-      <Header />
-    </div>
-  );
+  return <div className="layer">HELLO WORLD</div>;
 }
 
 export default App;
