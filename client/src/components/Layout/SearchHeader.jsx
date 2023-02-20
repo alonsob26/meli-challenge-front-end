c570d852a2374b39b03bd7324948f25067c949f7
Alonso <alonso.burgos.astorga@gmail.com>
2023-02-19 20:39:13 -0700

    [feat]: search bar header was created pixel perfect

--- client/src/components/Layout/SearchHeader.jsx
+++ client/src/components/Layout/SearchHeader.jsx
@@ -1,9 +1,11 @@
-import { Search } from "../SearchItems";
+import { Search } from "./SearchItems";
+import logo_ml from "../../assets/Logo_ML.png";
 
 export const Header = () => {
   return (
-    <div className="header_container">
+    <div className="searchHeader_container">
       <div>
+        <img className="searchHeader_logoML" src={logo_ml} alt="logo_ml" />
         <Search />
       </div>
     </div>
